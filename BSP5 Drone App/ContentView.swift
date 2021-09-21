0458195df3cbd7bda511c8e8b76945495cd01881
Flavio <flavio8699@gmail.com>
2021-09-21 17:32:18 +0200

    Initial Commit

---
+++ BSP5 Drone App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BSP5 Drone App
+//
+//  Created by Flavio Matias on 21/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
