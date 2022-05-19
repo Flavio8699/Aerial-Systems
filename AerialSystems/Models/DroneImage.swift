71b5d1b6f5cb463aafeca349616af978027a312e
Flavio <flavio8699@gmail.com>
2022-05-19 17:43:08 +0200

    Test

---
+++ AerialSystems/Models/DroneImage.swift
@@ -0,0 +1,13 @@
+//
+//  DroneImage.swift
+//  Aerial Systems
+//
+//  Created by Flavio Matias on 21/04/2022.
+//
+
+import Foundation
+
+struct DroneImage: Hashable {
+    var name: String
+    var data: Data?
+}
